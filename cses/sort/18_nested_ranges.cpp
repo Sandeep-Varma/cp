f76a77fea26de61e33d9dc582637899520769bd3
Sandeep-Varma <dsv6799@gmail.com>
2022-07-24 23:42:30 +0530

    Commit25 added readme to cses sort

--- cses/sort/18_nested_ranges.cpp
+++ cses/sort/18_nested_ranges.cpp
@@ -5,7 +5,11 @@ int main(){
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 	int n; cin>>n;
+	vector<vector<int>> v(n,vector<int>(2));
+	vector<bool> b(n,false);
+	for (int i=0; i<n; i++) cin>>v[i][0]>>v[i][1];
 	for (int i=0; i<n; i++){
+		if (b[i]) cout<<1;
 		
 	}
 }
