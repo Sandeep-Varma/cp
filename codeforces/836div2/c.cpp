0742c5fd95f6206f9ff8c34e52dd4dc7093a08d2
Sandeep-Varma <dsv6799@gmail.com>
2022-12-09 20:17:59 +0530

    Commit34 codeforces 836div2

--- codeforces/836div2/c.cpp
+++ codeforces/836div2/c.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Function to find the smallest divisor
+int smallestDivisor(int n)
+{
+	for (int i = 2; i <= sqrt(n); i += 2) {
+		if (n % i == 0) return i;
+	}
+	return n;
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -10,15 +19,23 @@ int main(){
 		if (n == x){
 			cout<<n<<" ";
 			for (int i=2; i<n; i++) cout<<i<<" ";
-			cout<<1<<endl;
+			cout<<1<<" "<<endl;
 		}
 		else if (n%x) cout<<-1<<endl;
 		else{
 			cout<<x<<" ";
-			for (int i=2; i<x; i++) cout<<i<<" ";
+			int i;
+			for (i=2; i<x; i++) cout<<i<<" ";
+			int div = smallestDivisor(n/x);
+			while(div != n/x){
+				cout<<x*div<<" ";
+				for (i=x+1; i<x*div; i++) cout<<i<<" ";
+				x = x*div;
+				div = smallestDivisor(n/x);
+			}
 			cout<<n<<" ";
 			for (int i=x+1; i<n; i++) cout<<i<<" ";
-			cout<<1<<endl;
+			cout<<1<<" "<<endl;
 		}
 	}
 }
