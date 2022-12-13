c9a446c2b4ac836768e408273fbcc3e52412a653
Sandeep-Varma <dsv6799@gmail.com>
2022-12-13 10:56:22 +0530

    Commit41 cses sort

--- cses/sort/18_nested_ranges.cpp
+++ cses/sort/18_nested_ranges.cpp
@@ -1,15 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool sort_ranges(vector<int> &v1, vector<int> &v2){
+	return (v1[0] < v2[0]) || (v1[0] == v2[0] && v1[1] > v2[0]);
+}
+
+bool check(vector<int> &v1, vector<int> &v2){
+	return (v1[0] <= v2[0]) && (v1[1] >= v2[1]);
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 	int n; cin>>n;
 	vector<vector<int>> v(n,vector<int>(2));
-	vector<bool> b(n,false);
 	for (int i=0; i<n; i++) cin>>v[i][0]>>v[i][1];
-	for (int i=0; i<n; i++){
-		if (b[i]) cout<<1;
-		
+	sort(v.begin(),v.end(),sort_ranges);
+	for (auto x:v){
+		for (auto y:x) cout<<y<<" ";
+		cout<<endl;
+	}
+	vector<vector<bool>> a(2,vector<bool>(n,false));
+	for (int i=0; i<n-1; i++){
+		if (check(v[i],v[i+1])){
+			a[0][i] = 1;
+			a[1][i+1] = 1;
+			if (v[i] == v[i+1]){
+				a[1][i] = 1;
+				a[0][i+1] = 1;
+			}
+		}
+	}
+	for (auto x:a){
+		for (auto y:x) cout<<y<<" ";
+		cout<<endl;
 	}
 }
